c3ce526715b9e4c4551483749dbaa3ed9be003cf
anonfunc <anonfunc@gmail.com>
2013-12-14 23:15:50 -0800

    New CLI commands. One starts an activity, and one creates a client and then dumps you into a python shell to play with it.

--- harmony/__main__.py
+++ harmony/__main__.py
@@ -10,6 +10,15 @@ import sys
 from harmony import auth
 from harmony import client as harmony_client
 
+import code
+
+class EmbeddedConsole(code.InteractiveConsole):
+  def start(self):
+    try:
+      self.interact("Debug console starting...")
+    except:
+      print("Debug console closing...")
+
 
 def login_to_logitech(args):
     """Logs in to the Logitech service.
@@ -41,6 +50,35 @@ def show_config(args):
     client.disconnect(send_close=True)
     return 0
 
+def repl(args):
+    """Connects to the Harmony and start ipython with client"""
+    token = login_to_logitech(args)
+    client = harmony_client.create_and_connect_client(
+        args.harmony_ip, args.harmony_port, token)
+    console = EmbeddedConsole(locals())
+    console.start()
+    client.disconnect(send_close=True)
+    return 0
+
+def start_activity(args):
+    """Connects to the Harmony and starts an activity"""
+    token = login_to_logitech(args)
+    client = harmony_client.create_and_connect_client(
+        args.harmony_ip, args.harmony_port, token)
+    config = client.get_config()
+    activities = config['activity']
+    labels_and_ids = dict([(a['label'], a['id']) for a in activities])
+    matching = [label for label in labels_and_ids.keys()
+                if args.activity.lower() in label.lower()]
+    if len(matching) == 1:
+        activity = matching[0]
+        print "Found activity named %s (id %s)" % (activity,
+                                                   labels_and_ids[activity])
+        client.start_activity(labels_and_ids[activity])
+    else:
+        print "found too many! %s" % (" ".join(matching))
+    client.disconnect(send_close=True)
+    return 0
 
 def main():
     """Main method for the script."""
@@ -70,6 +108,14 @@ def main():
         'show_config', help='Print the Harmony device configuration.')
     list_devices_parser.set_defaults(func=show_config)
 
+    repl_parser = subparsers.add_parser(
+        'repl', help='Start a client repl')
+    repl_parser.set_defaults(func=repl)
+
+    start_activity_parser = subparsers.add_parser(
+        'start', help='Start an activity whose name contains arg')
+    start_activity_parser.set_defaults(func=start_activity)
+    start_activity_parser.add_argument("activity", help="String to match activity")
     args = parser.parse_args()
 
     logging.basicConfig(
