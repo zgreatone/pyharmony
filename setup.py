9b66035f4d2013b35486b9f835e7ed42243da857
Bennett Kanuka <bkanuka@gmail.com>
2015-09-28 16:48:34 -0400

    better setup.py and rsctructure

--- setup.py
+++ setup.py
@@ -1,6 +1,55 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+here = lambda *a: os.path.join(os.path.dirname(__file__), *a)
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open(here('README.md')).read()
+requirements = [x.strip() for x in open(here('requirements.txt')).readlines()]
+
 setup(
-    name = "PyHarmony",
-    version = "0.1",
-    packages = find_packages(),
+    name='pyharmony',
+    version='0.1.0',
+    description='',
+    long_description=readme,
+    author='Bennett Kanuka',
+    author_email='bkanuka@gmail.com',
+    url='https://github.com/bkanuka/pyharmony',
+    packages=[
+        'pyharmony',
+    ],
+    package_dir={'pyharmony': 'pyharmony'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='pyharmony',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Home Automation',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'harmony = pyharmony.__main__:main'
+        ]
+    },
 )
